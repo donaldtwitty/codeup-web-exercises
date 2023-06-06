a6f1982f4979b50e39937f6f8bd5fa4f4f2a220c
Donald Twitty <donaldctwittyjr@outlook.com>
2023-06-06 10:34:34 -0500

    for_loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,27 @@
-function showMultiplicationTable() {
-    const number = parseInt(prompt('Enter an integer: '));
-    for (let i = 1; i <= 10; i++) {
-        const result = i * number;
-        console.log(`${number} * ${i} = ${result}`);
-    }
+// function showMultiplicationTable() {
+//     const number = parseInt(prompt('Enter an integer: '));
+//     for (let i = 1; i <= 10; i++) {
+//         const result = i * number;
+//         console.log(`${number} * ${i} = ${result}`);
+//     }
+// }
+// showMultiplicationTable();
+
+// for (let i = 0; i<=200; i++) {
+//     let i = Math.floor(Math.random() * 200) + 20;
+//     if (i % 2 === 0) {
+//         console.log(i + " is even");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
+
+for () {
+    console.log()
 }
-showMultiplicationTable();
+
+
+
+// for (let i = 5; i<=100; i+=5){
+//     console.log(i)
+// }
