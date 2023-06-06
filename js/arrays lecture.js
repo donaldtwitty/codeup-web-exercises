6ece8445c73ab936c3e564ff74ae9ca9e9b3b57c
Donald Twitty <donaldctwittyjr@outlook.com>
2023-06-06 15:26:55 -0500

    arrays lecture and exercise

---
+++ js/arrays lecture.js
@@ -0,0 +1,14 @@
+const names = ["Donald", "Pooh Shiesty", "Big30"]
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+// console.log(names[names.length - 1]);
+
+// for(let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+names.forEach((name) => {
+    console.log(name);
+})
