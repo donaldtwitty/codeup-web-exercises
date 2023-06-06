49f460ec79757966bed3c838607eff2d8bda77fd
Donald Twitty <donaldctwittyjr@outlook.com>
2023-06-06 10:41:44 -0500

    break_and_continue

---
+++ js/break_and_continue.js
@@ -0,0 +1,14 @@
+let numberToStopAt = 5;
+
+for (let i = 1; i < 100; i++) {
+
+    console.log('Loop counter is: ' + i);
+
+    if (i === numberToStopAt) {
+        console.log('We have reached the stopping point: break!');
+        // use the break keyword to exit from the while loop
+        break;
+        // nothing after the break will get processed
+        console.log('You will never see this line.');
+    }
+}
