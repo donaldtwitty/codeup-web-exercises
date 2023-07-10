f98d63cca996b4d2402ca34b31fa776675a88713
Donald Twitty <donaldctwittyjr@outlook.com>
2023-07-10 11:39:49 -0500

    mouse events exercise - added preferred method

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,4 +1,8 @@
 "use strict"
+/*Preferred Method
+$('h1').on('click', (e) => {
+        $(e.target).css('background', 'rebeccapurple');
+}); */
 $("h1").click(function (){
         $(this).css('background-color', 'rebeccapurple')
 });
