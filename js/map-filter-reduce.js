3d449b4c7ac4b21dc6c6e0ab419528c9d8e68d2f
Donald Twitty <donaldctwittyjr@outlook.com>
2023-07-20 13:57:26 -0500

    map filter reduce exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -1,5 +1,53 @@
-(()=>{
-    //map
-    fetch()
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
 
-})();
+// TODO Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+const usersWithThreeLanguages = users.filter(user => {
+    return user.languages.length >= 3;
+});
+
+console.log(usersWithThreeLanguages);
+
+// TODO Use .map to create an array of strings where each element is a user's email address.
+const userEmails = users.map(user => {
+    return user.email;
+});
+
+console.log(userEmails);
+
+// TODO Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
